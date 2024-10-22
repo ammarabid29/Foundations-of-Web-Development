be152c33bb498bbabb95c82e96edcd8e29d45a7d
ammarabid29 <aammarawan786@gmail.com>
2024-10-22 13:02:31 +0500

    Different Components of React

---
+++ react/my-webapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="content"></div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/my-webapp/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <h1>The Coder Web App</h1>
+            <div className="links">
+                <a href="/"> Home</a>
+                <a href="/create" >New App</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
