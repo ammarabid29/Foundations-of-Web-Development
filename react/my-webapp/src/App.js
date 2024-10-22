fdf0c4c5d526466e4be5057e4ff2c020dd88deb0
ammarabid29 <aammarawan786@gmail.com>
2024-10-22 13:23:36 +0500

    useState Hook

--- react/my-webapp/src/App.js
+++ react/my-webapp/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to new Web App";
-  const visits = 50;
-  // const employee = { name: "Ammar", age: 22 };
-  const link = 'http://www.google.com'
 
   return (
     <div className="App" >
+      <Navbar></Navbar>
       <div className="content"></div>
-      <h1>{title}</h1>
-      <p>Visited {visits} times</p>
-      {/* <p>{employee}</p> //error  */}
-      <p>{10}</p>
-      <p>{"Hello Coder"}</p>
-      <p>{[1, 2, 3, 4, 5]}</p>
-      <p>{Math.random() * 10}</p>
-      <a href={link}>Google Site</a>
+      <Home></Home>
     </div>
   );
 }
--- react/my-webapp/src/Home.js
+++ react/my-webapp/src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+
 const Home = () => {
+
+    const [name, setName] = useState("Ammar");
+
+    const handleClick = () => {
+        setName("Abid");
+    }
+
     return (
         <div className="Home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <p>{name}</p>
         </div>
     );
 }
