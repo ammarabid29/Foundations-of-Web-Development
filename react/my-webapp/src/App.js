d0b39c204f96801809d796daf62d86aff96b15ec
ammarabid29 <aammarawan786@gmail.com>
2024-10-22 13:47:02 +0500

    React props

--- react/my-webapp/src/App.js
+++ react/my-webapp/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
 
   return (
     <div className="App" >
-      <Navbar></Navbar>
       <div className="content"></div>
       <Home></Home>
     </div>
--- react/my-webapp/src/Home.js
+++ react/my-webapp/src/Home.js
@@ -1,25 +1,46 @@
-import { useState } from "react";
-
 
 const Home = () => {
-
-    const [count, setCount] = useState(0);
-
-    const increaseCount = () => {
-        setCount(count + 1);
-    }
-    const decreaseCount = () => {
-        setCount(count - 1);
-    }
-
     return (
         <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={increaseCount}>Click to Increase</button>
-            <p>Count is: {count}</p>
-            <button onClick={decreaseCount}>Click to Decrease</button>
+            <h2>Homepage</h2><hr />
+            <ExampleTwo Name="Ammar" Age="22" Job="Front-End"/>
+            <ExampleTwo Name="Abid" Age="55" Job="Teacher"/>
+            <ExampleTwo Name="Shahzaib" Age="23" Job="Lawyer"/>
         </div>
     );
 }
+
+const ExampleTwo = (props) => {
+    return (
+        <div>
+            <h3>Name: {props.Name}</h3>
+            <h3>Age: {props.Age}</h3>
+            <h3>Job: {props.Job}</h3><hr />
+        </div>
+    );
+}
+
+// const Example = () => {
+//     return (
+//    // Hard-Coded Data 
+//       <div>
+//         <div>
+//           <h3>Name: Ammar</h3>
+//           <h3>Age: 22</h3>
+//           <h3>Job: Front-End</h3><hr />
+//         </div>
+//         <div>
+//           <h3>Name: Abid</h3>
+//           <h3>Age: 55</h3>
+//           <h3>Job: Teacher</h3><hr />
+//         </div>
+//         <div>
+//           <h3>Name: Shahzaib</h3>
+//           <h3>Age: 23</h3>
+//           <h3>Job: Lawyer</h3><hr />
+//         </div>
+//       </div>
+//     );
+// }
 
 export default Home;
