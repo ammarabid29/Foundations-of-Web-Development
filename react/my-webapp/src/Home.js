bf1524569f42b03a28c83aa77c62ed4705373ad6
ammarabid29 <aammarawan786@gmail.com>
2024-10-22 14:20:33 +0500

    React props quiz

--- react/my-webapp/src/Home.js
+++ react/my-webapp/src/Home.js
@@ -2,45 +2,23 @@
 const Home = () => {
     return (
         <div className="Home">
-            <h2>Homepage</h2><hr />
-            <ExampleTwo Name="Ammar" Age="22" Job="Front-End"/>
-            <ExampleTwo Name="Abid" Age="55" Job="Teacher"/>
-            <ExampleTwo Name="Shahzaib" Age="23" Job="Lawyer"/>
+            <Library Title="CSS" Author="David" Description="Beginers to Advance Level" Date="2004"></Library>
+            <Library Title="Java" Author="Varner" Description="A Complete Guide" Date="2008"></Library>
         </div>
     );
 }
 
-const ExampleTwo = (props) => {
+
+
+const Library = (props) => {
     return (
-        <div>
-            <h3>Name: {props.Name}</h3>
-            <h3>Age: {props.Age}</h3>
-            <h3>Job: {props.Job}</h3><hr />
+        <div className="Card">
+            <h2>{props.Title}</h2> <hr />
+            <h3>{props.Author}</h3> <hr />
+            <p>{props.Description}</p> <br /><hr />
+            <p>{props.Date}</p><hr />
         </div>
     );
 }
-
-// const Example = () => {
-//     return (
-//    // Hard-Coded Data 
-//       <div>
-//         <div>
-//           <h3>Name: Ammar</h3>
-//           <h3>Age: 22</h3>
-//           <h3>Job: Front-End</h3><hr />
-//         </div>
-//         <div>
-//           <h3>Name: Abid</h3>
-//           <h3>Age: 55</h3>
-//           <h3>Job: Teacher</h3><hr />
-//         </div>
-//         <div>
-//           <h3>Name: Shahzaib</h3>
-//           <h3>Age: 23</h3>
-//           <h3>Job: Lawyer</h3><hr />
-//         </div>
-//       </div>
-//     );
-// }
 
 export default Home;
